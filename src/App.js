82987c12301818ddadbb27e87e0d8a0ef216426f
Ephriam Henderson <orik162@gmail.com>
2019-11-04 10:23:28 -0600

    First Commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,8 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload. <br></br>
+          HTML in JavaScript is weird.
         </p>
         <a
           className="App-link"
